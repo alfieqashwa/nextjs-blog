d667e439670a6a990341816a23baa2596de6c246
alfieqashwa <alfieqashwa@gmail.com>
2020-04-25 17:55:24 +0700

    edited name

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Alfie Qashwa";
+const name = "Cello El-Z";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
